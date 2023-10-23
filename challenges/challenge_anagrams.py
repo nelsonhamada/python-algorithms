46587f42c8a2c46985938ae2b3be1e39372f638b
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-10-23 19:28:19 -0300

    [Feat]: is_anagram implementada.

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,41 @@
+def merge_sort(numbers, start=0, end=None):
+    if end is None:
+        end = len(numbers)
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(numbers, start, mid)
+        merge_sort(numbers, mid, end)
+        merge(numbers, start, mid, end)
+
+
+def merge(numbers, start, mid, end):
+    left = numbers[start:mid]
+    right = numbers[mid:end]
+
+    left_index, right_index = 0, 0
+
+    for general_index in range(start, end):
+        if left_index >= len(left):
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+        elif right_index >= len(right):
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        elif left[left_index] < right[right_index]:
+            numbers[general_index] = left[left_index]
+            left_index = left_index + 1
+        else:
+            numbers[general_index] = right[right_index]
+            right_index = right_index + 1
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código  aqui."""
-    raise NotImplementedError
+    if not first_string and not second_string:
+        return ("", "", False)
+    formatFirst = list(first_string.lower())
+    formatSecond = list(second_string.lower())
+    merge_sort(formatFirst, 0, len(formatFirst))
+    merge_sort(formatSecond, 0, len(formatSecond))
+    if formatFirst == formatSecond:
+        return ("".join(formatFirst), "".join(formatSecond), True)
+    return ("".join(formatFirst), "".join(formatSecond), False)
