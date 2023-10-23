cc99ccc3e96c5bb511f97333db836096a5b8fc9c
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-10-23 18:41:02 -0300

    [Feat]: challenge_palindromes_recursive.py

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    if high_index >= 3 and word[low_index] == word[high_index]:
+        wordSliced = word[(low_index + 1): high_index]
+        return is_palindrome_recursive(wordSliced, 0, len(wordSliced) - 1)
+    if high_index >= 0 and word[low_index] == word[high_index]:
+        return True
+    return False
