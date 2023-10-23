0bca32cf012126bfb71235e01d06335aa1e170f2
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-10-23 13:05:11 -0300

    [Feat]: study_schedule implementada.

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    if not target_time:
+        return None
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+        if target_time >= period[0] and target_time <= period[1]:
+            count += 1
+    return count
