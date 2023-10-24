6b7d507dac454376953b403562d94a01ed1da9c8
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-10-24 10:05:14 -0300

    [Feat]: is_palindromes_iterative implementada.

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,9 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    if len(word) == 1:
+        return True
+    reversed = word[::-1]
+    if reversed == word:
+        return True
+    return False
