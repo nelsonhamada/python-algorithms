40a880dd779e839710f0fdad5689b4fa7cee1409
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-10-24 10:31:23 -0300

    [Test]: Testes de encrypt_message implementados.

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("message", 7) == "egassem"
+    assert encrypt_message("message", 1) == "m_egasse"
+    assert encrypt_message("message", 2) == "egass_em"
+    assert encrypt_message("message", 3) == "sem_egas"
+    assert encrypt_message("message", 4) == "ega_ssem"
+
+    with pytest.raises(TypeError, match='tipo inválido para message'):
+        encrypt_message(None, 4)
+
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message("message", "3")
