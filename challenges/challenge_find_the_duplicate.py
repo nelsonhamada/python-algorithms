e469fa43c854cc10a2b111d94ff3bd100f3aac65
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-10-24 09:46:08 -0300

    [Feat]: fint_the_duplicate implementada.

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    result = False
+    nums.sort()
+    for index in range(0, len(nums) - 1):
+        if isinstance(nums[index], str) or nums[index] < 0:
+            result = False
+            break
+        if nums[index] == nums[index + 1]:
+            result = nums[index]
+            break
+    return result
