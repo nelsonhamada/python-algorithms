8cda5eb81e5801e4b602a6330d5cc3830c92ca74
Nelson Hamada <fumiyuki.hamada@gmail.com>
2023-11-13 11:23:58 -0300

    [Feat]: Refatorando funções para diminuir complexidade.

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,9 +1,4 @@
 def is_palindrome_iterative(word):
     if not word:
         return False
-    if len(word) == 1:
-        return True
-    reversed = word[::-1]
-    if reversed == word:
-        return True
-    return False
+    return word[::-1] == word
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,4 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if not word:
-        return False
     if high_index >= 3 and word[low_index] == word[high_index]:
         wordSliced = word[(low_index + 1): high_index]
         return is_palindrome_recursive(wordSliced, 0, len(wordSliced) - 1)
